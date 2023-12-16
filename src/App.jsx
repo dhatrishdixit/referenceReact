7484a9d8bc03202f35c072596605fe36b05922be
dhatrishdixit <dhatrish29@gmail.com>
2023-12-16 11:31:27 +0530

    third commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import { useState } from 'react'
 
 import Form from './components/Form'
+import Wrapper from './components/Wrapper'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-     <Form />
+    <Wrapper>
+     <Form /></Wrapper>
     </>
   )
 }
--- src/components/Wrapper.jsx
+++ src/components/Wrapper.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Wrapper({children}) {
   return (
-    <div>{children}</div>
+    <div style={{backgroundColor:"yellow" ,border:"1px solid red",padding:"5px"}}>{children}</div>
   )
 }
 
